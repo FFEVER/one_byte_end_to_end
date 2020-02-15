368d1d35c33496fea1466f88ad077de1b7a0751a
Nattaphol <rootncdev2015@gmail.com>
2020-02-16 00:04:45 +0700

    Add simple poll web

---
+++ one_byte_end_to_end/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for one_byte_end_to_end project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'one_byte_end_to_end.settings')
+
+application = get_asgi_application()
