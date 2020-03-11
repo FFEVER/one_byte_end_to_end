8e3ab33df0ae9447b95aebe15f4176bb98f514e6
Nattaphol <rootncdev2015@gmail.com>
2020-03-11 22:38:03 +0700

    Add login url api/login

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from user_auth.views import UserRegistrationView
+from user_auth.views import UserLoginView
 
 
 urlpatterns = [
     url(r"^signup", UserRegistrationView.as_view()),
+    url(r"^signin", UserLoginView.as_view()),
 ]
 
